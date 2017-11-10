64abd0b7a0a7c20e387d874bdc9fde2ce014b941
Florentin <menage.florentin@gmail.com>
2017-11-10 11:35:27 +0100

    Rajout d'une classe intermédiaire pr l'ihm

---
+++ src/animation/IhmUnTerritoire.java
@@ -0,0 +1,17 @@
+package animation;
+import javax.swing.*;
+
+public class IhmUnTerritoire extends JPanel {
+	
+	JProgressBar jaugeEau;
+	int minEau;
+	int maxEau;
+	
+	JProgressBar jaugeVeg;
+	int minVeg;
+	int maxVeg;
+	
+	public IhmUnTerritoire() {
+		
+	}
+}
